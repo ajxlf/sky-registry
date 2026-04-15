119bea2b13710967dee31923652ab916d2884abb
ajxlf <aj.ferreira@hotmail.co.uk>
2026-04-15 02:58:44 +0100

    Improve org UI and department card layout

--- organisation/admin.py
+++ organisation/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from .models import Department, Team, Dependency
 
+
 class TeamInline(admin.TabularInline):
     model = Team
     extra = 0
 
+
 class DependencyInline(admin.TabularInline):
     model = Dependency
     extra = 0
+
 
 @admin.register(Department)
 class DepartmentAdmin(admin.ModelAdmin):
@@ -15,11 +18,13 @@ class DepartmentAdmin(admin.ModelAdmin):
     search_fields = ("name", "leader", "specialisation")
     inlines = [TeamInline, DependencyInline]
 
+
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
     list_display = ("name", "manager", "status", "department")
     list_filter = ("status", "department")
     search_fields = ("name", "manager", "department__name")
+
 
 @admin.register(Dependency)
 class DependencyAdmin(admin.ModelAdmin):
