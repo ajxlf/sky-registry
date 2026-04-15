0b8c1ce9a088ebda4cae2131291e15ae55cb6e04
ajxlf <aj.ferreira@hotmail.co.uk>
2026-04-15 02:22:04 +0100

    Register models in Django admin

--- organisation/admin.py
+++ organisation/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Department, Team, Dependency
 
-# Register your models here.
+class TeamInline(admin.TabularInline):
+    model = Team
+    extra = 0
+    fields = ('name', 'manager', 'status')
+
+class DependencyInline(admin.TabularInline):
+    model = Dependency
+    extra = 0
+    fields = ('description',)
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'leader', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('name', 'leader', 'specialisation')
+    inlines = [TeamInline, DependencyInline]
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'manager', 'status', 'department', 'get_team_count')
+    list_filter = ('status', 'department')
+    search_fields = ('name', 'manager')
+    list_select_related = ('department',)
+
+    def get_team_count(self, obj):
+        return f"{obj.department.name}"
+    get_team_count.short_description = 'Department'
+
+admin.site.register(Dependency)
