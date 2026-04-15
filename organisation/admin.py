0d68bc6da6668b8609b35352cc2cde46163ec357
ajxlf <aj.ferreira@hotmail.co.uk>
2026-04-15 02:54:38 +0100

    Clean admin reg and switch org templates

--- organisation/admin.py
+++ organisation/admin.py
@@ -4,29 +4,25 @@ from .models import Department, Team, Dependency
 class TeamInline(admin.TabularInline):
     model = Team
     extra = 0
-    fields = ('name', 'manager', 'status')
 
 class DependencyInline(admin.TabularInline):
     model = Dependency
     extra = 0
-    fields = ('description',)
 
 @admin.register(Department)
 class DepartmentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'leader', 'created_at')
-    list_filter = ('created_at',)
-    search_fields = ('name', 'leader', 'specialisation')
+    list_display = ("name", "leader", "created_at")
+    search_fields = ("name", "leader", "specialisation")
     inlines = [TeamInline, DependencyInline]
 
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
-    list_display = ('name', 'manager', 'status', 'department', 'get_team_count')
-    list_filter = ('status', 'department')
-    search_fields = ('name', 'manager')
-    list_select_related = ('department',)
+    list_display = ("name", "manager", "status", "department")
+    list_filter = ("status", "department")
+    search_fields = ("name", "manager", "department__name")
 
-    def get_team_count(self, obj):
-        return f"{obj.department.name}"
-    get_team_count.short_description = 'Department'
-
-admin.site.register(Dependency)
+@admin.register(Dependency)
+class DependencyAdmin(admin.ModelAdmin):
+    list_display = ("description", "department")
+    list_filter = ("department",)
+    search_fields = ("description", "department__name")
