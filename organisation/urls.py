ddb1ed7a8bd8f6af53749512cee61ec9795c6c1f
ajxlf <aj.ferreira@hotmail.co.uk>
2026-04-27 18:52:42 +0100

    Remove redundant link in urls.py

--- organisation/urls.py
+++ organisation/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path, include
 from . import views
 
-app_name = 'organisation'
+app_name = "organisation"
 
 urlpatterns = [
-    path('', views.department_list, name='department_list'),
-    path('department/<int:pk>/', views.department_detail, name='department_detail'),
+    path("", views.department_list, name="department_list"),
+    path("department/<int:pk>/", views.department_detail, name="department_detail"),
     path("teams/", views.teams, name="teams"),
-
-    path('messages/', include(('messaging.urls', 'messaging'), namespace='messaging')),
 ]
