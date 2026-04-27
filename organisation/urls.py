861f9c21182607deb8e92c719028711320ed90e4
Aiman Chowdhury <grufelonios@gmail.com>
2026-04-27 12:52:52 +0100

    Update organisation URLs configuration

--- organisation/urls.py
+++ organisation/urls.py
@@ -6,6 +6,7 @@ app_name = 'organisation'
 urlpatterns = [
     path('', views.department_list, name='department_list'),
     path('department/<int:pk>/', views.department_detail, name='department_detail'),
+    path("teams/", views.teams, name="teams"),
 
     path('messages/', include(('messaging.urls', 'messaging'), namespace='messaging')),
 ]
