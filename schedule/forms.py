040c186f01b4127d4d723175287d78714d36ed35
ajxlf <aj.ferreira@hotmail.co.uk>
2026-04-28 23:43:44 +0100

    Format 'New Meeting' box for better aesthetics

--- schedule/forms.py
+++ schedule/forms.py
@@ -15,7 +15,24 @@ class MeetingForm(forms.ModelForm):
             "agenda",
         ]
         widgets = {
-            "scheduled_for": forms.DateTimeInput(attrs={"type": "datetime-local"}),
-            "agenda": forms.Textarea(attrs={"rows": 4}),
+            "title": forms.TextInput(
+                attrs={"class": "form-input", "autocomplete": "off"}
+            ),
+            "team": forms.Select(attrs={"class": "form-input"}),
+            "organiser_name": forms.TextInput(
+                attrs={"class": "form-input", "autocomplete": "name"}
+            ),
+            "scheduled_for": forms.DateTimeInput(
+                attrs={"type": "datetime-local", "class": "form-input"}
+            ),
+            "duration_minutes": forms.NumberInput(
+                attrs={"class": "form-input", "min": 1}
+            ),
+            "agenda": forms.Textarea(
+                attrs={
+                    "rows": 4,
+                    "class": "form-input",
+                    "placeholder": "Optional agenda or notes",
+                }
+            ),
         }
-
