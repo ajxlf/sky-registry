2b95401b9a0c70e1aa3f00eae3012c437525db69
ajxlf <aj.ferreira@hotmail.co.uk>
2026-04-28 17:06:22 +0100

    organisation/apps.py - small tweak for consistency with other apps.py files

--- organisation/apps.py
+++ organisation/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class OrganisationConfig(AppConfig):
-    name = 'organisation'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "organisation"
